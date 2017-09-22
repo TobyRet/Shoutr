2525e51ab97f53da664b5e066e4a443cf89e356b
Toby Retallick <toby.retallick@gmail.com>
2017-09-22 16:50:48 +0100

    efactor application to be polymorphic

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,15 +1,25 @@
 class ShoutsController < ApplicationController
   def create
     @shout = current_user.shouts.create(shout_params)
-    redirect_to root_path, redirect_options_for(shout)
+    redirect_to root_path, redirect_options_for(@shout)
   end
 
   def show
     @shout = Shout.find(params[:id])
   end
 
+  private
+
   def shout_params
-    params.require(:shout).permit(:body)
+    { content: content_from_params }
+  end
+
+  def content_from_params
+    TextShout.new(content_params)
+  end
+
+  def content_params
+    params.require(:shout).require(:content).permit(:body)
   end
 
   def redirect_options_for(shout)
