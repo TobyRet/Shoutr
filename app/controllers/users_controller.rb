d83c77bb2275dbd6b89c62b422ddaf27138b8386
Toby Retallick <toby.retallick@gmail.com>
2017-09-23 23:23:43 +0100

    Fix sign up and null false

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < Clearance::UsersController
-  private
-
   def show
     @user = User.find(params[:id])
     @shouts = @user.shouts
   end
+  
+  def new
+    @user = User.new
+  end
+ 
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922144512) do
+ActiveRecord::Schema.define(version: 20170923221938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shouts", force: :cascade do |t|
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "content_type"
-    t.integer "content_id"
+    t.string "content_type", null: false
+    t.integer "content_id", null: false
     t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id"
     t.index ["user_id"], name: "index_shouts_on_user_id"
   end
