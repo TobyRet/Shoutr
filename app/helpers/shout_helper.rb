c2b0503da4d3414564b5e2a1bf9f841e4d1d6144
Toby Retallick <toby.retallick@gmail.com>
2017-10-03 12:36:10 +0100

    Move avatar method to AvatarHelper

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,10 +1,4 @@
 module ShoutHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest user.email
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-    image_tag gravatar_url
-  end
-
   def like_button(shout)
     if current_user.liked?(shout)
       link_to 'Unlike', unlike_shout_path(shout), method: :delete
